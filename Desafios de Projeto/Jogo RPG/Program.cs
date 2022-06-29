1862f68297b208994097034579636ca82ee188f2
Lívia Morais <104389308+livmorais@users.noreply.github.com>
2022-06-29 17:44:42 -0300

    Add files via upload

---
+++ Desafios de Projeto/Jogo RPG/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Jogo_RPG.src.Entities;
+
+namespace Jogo_RPG
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Knight arus = new Knight("Arus", 23, "Knight");
+        Wizard wizard = new Wizard("Jennica", 23, "Wizard");
+
+        Console.WriteLine(wizard.Attack(1));
+        Console.WriteLine(wizard.Attack(7));
+        }
+    }
+}
